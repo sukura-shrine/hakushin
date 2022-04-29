aea0385b877cbe785ba1675b94b35802dc7f9a1c
hakushinring <hakushinring@gmail.com>
2022-04-29 19:19:52 +0800

    chore: update

--- packages/shrine-cli/src/create-app.ts
+++ packages/shrine-cli/src/create-app.ts
@@ -1,16 +1,22 @@
 import fs from 'fs'
-import fspro from 'fs/promises'
 import path from 'path'
 import process from 'process'
-// import spawn from 'cross-spawn'
+import cp from 'child_process'
 
 export default async function (appName: string) {
   const driname = process.cwd()
   const projectName = path.join(driname, appName)
-  if (!fs.existsSync(projectName)) {
-    throw new Error('')
+  if (fs.existsSync(projectName)) {
+    throw new Error(`${projectName} 文件夹已经存在`)
   }
-  await fspro.cp('../../shrine-template', projectName)
 
-  // spawn.sync(`cd ${appName} & pnpm install`)
+  const args = ['clone', '-b', 'mobile', `https://github.com/sukura-shrine/app-template.git`, appName]
+  const child = cp.spawn('git', args, { stdio: 'inherit' })
+
+  child.on('close', (code: number) => {
+    console.log('close')
+    if (code !== 0) {
+      return;
+    }
+  })
 }
