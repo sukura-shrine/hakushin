47b4d8dc0cbbb39814289406f101135a4da1ba22
hakushinring <hakushinring@gmail.com>
2022-05-06 20:44:21 +0800

    chore: update

---
+++ packages/shrine-cli/bin/index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+require('../dist/shrine-cli.js')
---
+++ packages/shrine-cli/src/init.ts
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import cp from 'child_process'
+
+export default async function (projectName: string) {
+  if (fs.existsSync(projectName)) {
+    throw new Error(`${projectName} 文件夹已经存在`)
+  }
+
+  const args = ['clone', '-b', 'main', `https://github.com/sukura-shrine/app-template.git`, projectName]
+  const child = cp.spawn('git', args, { stdio: 'inherit' })
+
+  child.on('close', (code: number) => {
+    if (code !== 0) {
+      return console.error(`ERROR: code ${code}`)
+    }
+  })
+}
