a7ae79ab52eac49f95767757785e74ca9b88e994
hakushinring <hakushinring@gmail.com>
2022-07-12 19:58:42 +0800

    chore: update

--- packages/utils/__tests__/index.test.js
+++ packages/utils/__tests__/index.test.js
@@ -1,15 +1,52 @@
 import { describe, expect, test, vi } from 'vitest'
+import clientPakcagesInfo from '../src/client-packages-info'
 
-vi.mock('path', () => {
+const createPromiseIterator = (items) => {
   return {
-    default: { myDefaultKey: vi.fn() },
-    join: vi.fn(),
-    // etc...
+    [Symbol.asyncIterator]: () => {
+      return {
+        next: () => Promise.resolve({
+          done: items.length === 0,
+          value: items.shift()
+        })
+      }
+    }
+  }
+}
+
+const createFsDirent = ({ name, isDirectory }) => {
+  return {
+    name,
+    isDirectory () {
+      return isDirectory
+    }
+  }
+}
+
+const mockFsData = [
+  { name: 'app1', isDirectory: true },
+  { name: 'app2', isDirectory: true },
+  { name: 'app3', isDirectory: false },
+]
+
+vi.mock('fs/promises', () => {
+  return {
+    default: {
+      fsPromises: vi.fn()
+    },
+    opendir: vi.fn(async (dir) => {
+      const list = mockFsData.map(item => createFsDirent(item))
+      return createPromiseIterator(list)
+    }),
+    readFile: vi.fn(async (dir, options) => {
+      return { name: dir }
+    }),
   }
 })
 
 describe('person', () => {
   test('person is defined', () => {
-    expect(person).toBeDefined()
+    clientPakcagesInfo('/')
+    // expect(person).toBeDefined()
   })
 })
