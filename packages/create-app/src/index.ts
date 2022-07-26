4ff6e6c4d32f0ec1281f9fc413dea9a89b65d0ab
hakushinring <hakushinring@gmail.com>
2022-07-26 18:44:23 +0800

    feat: build

--- packages/create-app/src/index.ts
+++ packages/create-app/src/index.ts
@@ -5,6 +5,7 @@ import process from 'process'
 import cac from 'cac'
 import init from './init.js'
 import createApp from './create-app.js'
+import build from './build.js'
 import { clientConfig } from '@hakushin/utils'
 
 async function main () {
@@ -31,6 +32,12 @@ async function main () {
     .action(async (name, options) => {
       const { ref } = options
       await createApp(name, ref)
+    })
+
+  cli
+    .command('build <name>', '打包子应用')
+    .action(async (name) => {
+      await build(name)
     })
 
   try {
